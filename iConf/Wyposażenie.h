680c39e9cb4066697dd2c7aec61a553ed4c7e7ac
Karik <dk44312@zut.edu.pl>
2020-06-10 20:24:01 +0200

    Wyposażenie getter and setter

--- iConf/Wyposażenie.h
+++ iConf/Wyposażenie.h
@@ -12,4 +12,14 @@ public:
     bool usuñ(int Id);
     bool dodaj_do_pokoju(int Id_wyposa¿enia, int Id_pokoju);
     bool usuñ_z_pokoju(int Id);
+
+    int get_id() { return _id; }
+    std::string get_nazwa() { return _nazwa; }
+    int get_iloœæ() { return _iloœæ; }
+    std::string get_opis() { return _opis; }
+
+    void set_id(int id) { _id = id; }
+    void set_nazwa(std::string nazwa) { _nazwa = nazwa; }
+    void set_iloœæ(int iloœæ) { _iloœæ = iloœæ; }
+    void set_opis(std::string opis) { _opis = opis; }
 };
