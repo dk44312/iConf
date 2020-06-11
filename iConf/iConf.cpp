9cd994d6308668b1dd761bc7617c1d65d0b51752
Karik <dk44312@zut.edu.pl>
2020-06-11 14:15:47 +0200

    exit app and log out methods created

--- iConf/iConf.cpp
+++ iConf/iConf.cpp
@@ -10,13 +10,47 @@
 #include "Użytkownik.h"
 #include "Wyposażenie.h"
 
-bool administrator_menu(Użytkownik user)
+bool exit_application() {//return true-exit app, return false- dont exit app
+    char shut_down;
+    while (true) {
+        std::cout << "Czy napewno chcesz zamknac aplikacje?: \n T-tak\nN-nie ";
+        std::cin >> shut_down;
+        if (shut_down == 'T' || shut_down == 't') {
+            return true;
+        }
+        else if (shut_down == 'N' || shut_down == 'n') {
+            return false;
+        }
+        else {
+            std::cout << "Nieprawidlowy wybor! sprobuj jeszcze raz.";
+        }
+    }
+}
+
+bool log_out() {//return true- log out form account, return false dont log out from account
+    char log_out;
+    while (true) {
+        std::cout << "Czy napewno chcesz wylogować?: \n T-tak\nN-nie ";
+        std::cin >> log_out;
+        if (log_out == 'T' || log_out == 't') {
+            return true;
+        }
+        else if (log_out == 'N' || log_out == 'n') {
+            return false;
+        }
+        else {
+            std::cout << "Nieprawidlowy wybor! sprobuj jeszcze raz.";
+        }
+    }
+}
+
+bool administrator_menu(Użytkownik user) //return true dla zakoncz aplikacje, return false dla wylogowania
 {
     char choice;
     bool status = true;
     std::cout << "Witaj " << user.get_nazwa() << "!\n";
     while (status) {
-        std::cout << " 1) Zarzadzaj obszarami\n 2) Zarzadzaj uzytkownikami\n 3) Zarzadzaj spotkaniami\n 4) Zarzadzaj pokojami 5) Moje konto\n \n 6)Wyloguj";
+        std::cout << " 1) Zarzadzaj obszarami\n 2) Zarzadzaj uzytkownikami\n 3) Zarzadzaj spotkaniami\n 4) Zarzadzaj pokojami 5) Moje konto\n \n 6) Wyloguj\n 7) Zamknij aplikacje";
         std::cout << "wybierz odpowiednia opcje ";
         std::cin >> choice;
         switch (choice) {
@@ -31,20 +65,15 @@ bool administrator_menu(Użytkownik user)
         case'5':
             break;
         case'6':
-            char log_out;
-            bool finish = true;
-            while (finish) {
-                std::cout << "Czy napewno chcesz wylogować?: \n T-tak\nN-nie ";
-                std::cin >> log_out;
-                if (log_out == 'T' || log_out == 't') {
-                    return false;
-                }
-                else if (log_out == 'N' || log_out == 'n') {
-                    finish == false;
-                }
-                else {
-                    std::cout << "Nieprawidlowy wybor! sprobuj jeszcze raz.";
-                }
+            bool temp = log_out();
+            if (temp == true) {
+                return false;
+            }
+            break;
+        case'7':
+            bool temp = exit_application();
+            if (temp == true) {
+                return true;
             }
             break;
         default:
@@ -52,15 +81,16 @@ bool administrator_menu(Użytkownik user)
             break;
         }
     }
+    return true;
 }
 
-bool menadżer_menu(Użytkownik user)
+bool menadżer_menu(Użytkownik user)//return true dla zakoncz aplikacje, return false dla wylogowania
 {
     char choice;
     bool status = true;
     std::cout << "Witaj " << user.get_nazwa() << "!\n";
     while (status) {
-        std::cout << " 1) Przegladaj obszary\n 2) Przegladaj spotkania\n 3) Przegladaj pokoje\n 4) Moje konto\n 5)Wyloguj";
+        std::cout << " 1) Przegladaj obszary\n 2) Przegladaj spotkania\n 3) Przegladaj pokoje\n 4) Moje konto\n 5) Wyloguj\n 6) Zamknij aplikacje";
         std::cout << "wybierz odpowiednia opcje: ";
         std::cin >> choice;
         switch (choice) {
@@ -73,20 +103,15 @@ bool menadżer_menu(Użytkownik user)
         case'4':
             break;
         case'5':
-            char log_out;
-            bool finish = true;
-            while (finish) {
-                std::cout << "Czy napewno chcesz wylogować?: \n T-tak\nN-nie ";
-                std::cin >> log_out;
-                if (log_out == 'T' || log_out == 't') {
-                    return false;
-                }
-                else if (log_out == 'N' || log_out == 'n') {
-                    finish == false;
-                }
-                else {
-                    std::cout << "Nieprawidlowy wybor! sprobuj jeszcze raz.";
-                }
+            bool temp = log_out();
+            if (temp == true) {
+                return false;
+            }
+            break;
+        case'6':
+            bool temp = exit_application();
+            if (temp == true) {
+                return true;
             }
             break;
         default:
@@ -97,13 +122,13 @@ bool menadżer_menu(Użytkownik user)
     return true;
 }
 
-bool pracownik_menu(Użytkownik user)
+bool pracownik_menu(Użytkownik user)//return true dla zakoncz aplikacje, return false dla wylogowania
 {
     char choice;
     bool status=true;
     std::cout << "Witaj " << user.get_nazwa() << "!\n";
     while (status) {
-        std::cout << " 1) Przegladaj obszary\n 2) Przegladaj spotkania\n 3) Przegladaj pokoje\n 4) Moje konto\n 4)Wyloguj";
+        std::cout << " 1) Przegladaj obszary\n 2) Przegladaj spotkania\n 3) Przegladaj pokoje\n 4) Moje konto\n 5) Wyloguj\n 6) Zamknij aplikacje\n";
         std::cout << "wybierz odpowiednia opcje ";
         std::cin >> choice;
         switch (choice) {
@@ -116,20 +141,15 @@ bool pracownik_menu(Użytkownik user)
         case'4':
             break;
         case'5':
-            char log_out;
-            bool finish = true;
-            while (finish) {
-                std::cout << "Czy napewno chcesz wylogować?: \n T-tak\nN-nie ";
-                std::cin >> log_out;
-                if (log_out == 'T' || log_out == 't') {
-                    return false;
-                }
-                else if (log_out == 'N' || log_out == 'n') {
-                    finish == false;
-                }
-                else {
-                    std::cout << "Nieprawidlowy wybor! sprobuj jeszcze raz.";
-                }
+            bool temp = log_out();
+            if (temp == true) {
+                return false;
+            }
+            break;
+        case'6':
+            bool temp = exit_application();
+            if (temp == true) {
+                return true;
             }
             break;
         default:
@@ -137,13 +157,39 @@ bool pracownik_menu(Użytkownik user)
             break;
         }
     }
-    return false;
+    return true;
 }
 
-bool gość_menu() 
+bool gość_menu() //return true dla zakoncz aplikacje, return false dla zalogowania
 {
+    char choice;
+    bool status = true;
     std::cout << "Witaj użytkowniku!\n";
-    std::cout<< "1) Przegladaj obszary\n 2) Przegladaj Pokoje\n 3) Przegladaj Spotkania\n 4) Wyloguj\n"
+    while (status) {
+        std::cout << "1) Przegladaj obszary\n 2) Przegladaj Pokoje\n 3) Przegladaj Spotkania\n 4) Zaloguj\n 5) Zamknij aplikacje \n";
+        std::cout << "wybierz odpowiednia opcje: ";
+        std::cin >> choice;
+        switch (choice) {
+        case '1':
+            break;
+        case'2':
+            break;
+        case'3':
+            break;
+        case'4':
+            break;
+        case'5':
+            bool temp = exit_application();
+            if (temp == true) {
+                return true;
+            }
+            break;
+        default:
+            std::cout << "Nieprawidlowa opcja! Sprobuj jeszcze raz.\n";
+            break;
+        }
+    }
+    return true;
 }
 
 int main()
